fac83a43a1c3a31fce11dae2407209063b4ebf00
Michaela Hunt <mhunt@theprovatogroup.com>
2024-10-02 18:39:13 -0400

    Add question list, some comments

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 // TODO: Include packages needed for this application
-
+import inquirer from "inquirer";
+import fs from 'fs';
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is your GitHub username?",
+    "What is your email address?",
+    "What is your project's name?",
+    "Please write a short description of your project.",
+    "What kind of license should your project have?",
+    "What command should be run to run tests?",
+    "What does the user need to know about using the repo?",
+    "What does the user need to know about contributing to the repo?",
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, () => {});
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    //loop through and ask each question here? use generatemarkdown module to create data, send data to writeToFile
+}
 
 // Function call to initialize app
 init();
