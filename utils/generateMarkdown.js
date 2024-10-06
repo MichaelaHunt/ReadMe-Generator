8d378d0c26d53bf8783802df12ef16e162372df7
Michaela Hunt <mhunt@theprovatogroup.com>
2024-10-05 22:21:49 -0400

    Filled in render license sections

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,52 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) { }
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "MIT":
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    case "ISC":
+      return "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
+    case "Apache 2.0":
+      return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    case "GNU GPLv3":
+      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+    case "None":
+      return "";
+  };
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) { }
+function renderLicenseLink(license) {
+  switch (license) {
+    case "MIT":
+      return "https://opensource.org/licenses/MIT";
+    case "ISC":
+      return "https://opensource.org/licenses/ISC";
+    case "Apache 2.0":
+      return "https://opensource.org/licenses/Apache-2.0";
+    case "GNU GPLv3":
+      return "https://www.gnu.org/licenses/gpl-3.0";
+    case "None":
+      return "";
+  };
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { }
+function renderLicenseSection(license) {
+  if (license != "") {
+    return `This project is protected under the ${license} license.\nIf you'd like to learn more about this license, please visit: ${renderLicenseLink(license)}`;
+  }
+  else {
+    return "";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(license)}
 
   ## Description
 
@@ -41,13 +75,13 @@ function generateMarkdown(data) {
 
   ## License
 
-  ${data.licenseDetails}
+  ${renderLicenseSection(licenseDetails)}
 
   ## Features
 
   ${data.featureDetails}
 
-  ## How to Contribute
+  ## Contributing
 
   ${data.contributeDetails}
 
@@ -64,9 +98,7 @@ function generateMarkdown(data) {
 
 export default generateMarkdown;
 
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for information about my application repository
-// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+
 // WHEN I enter my project title
 // THEN this is displayed as the title of the README
 // WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
